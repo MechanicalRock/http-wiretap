54dfc761921a1be9af0b47d97255ae4e8e0e91f1
bryan-yu <bryan-500088@Hotmail.com>
2019-04-18 13:58:20 +0800

    add impl for scenarios

--- cucumber/steps/wiretap.steps.ts
+++ cucumber/steps/wiretap.steps.ts
@@ -1,87 +1,99 @@
-import { defineFeature, loadFeature } from "jest-cucumber";
+import { defineFeature, loadFeature } from "jest-cucumber"
+import fetch, { Response } from "node-fetch"
 
 const feature = loadFeature("cucumber/features/wiretap.feature")
+const proxyUrl = ""
 
 defineFeature(feature, scenario => {
+  let requestBody: string
+  let response: Response
 
   scenario("Post proxy is successfully sent", ({given, when, then}) => {
     given("we have an MVP proxy", () => {
+      //what do we do here?
     })
 
     given("a http post request body", () => {
-    });
+      requestBody = JSON.stringify({
+        message: `Test execution at timestamp: ${Date.now()}`,
+      })
+    })
 
-    when("we post the request to the MVP proxy endpoint successfully", () => {
+    when("we post the request to the MVP proxy endpoint successfully", async () => {
+      response = await fetch(`${proxyUrl}`, {
+        method: "POST",
+        body: requestBody
+      })
     })
 
     then("the return code will be 201", () => {
-      expect(true).toBe(false)
+      expect(response.status).toBe(201)
+    })
+  })
+
+  scenario('Post proxy fails', ({ given, and, when, then }) => {
+    given('we have an MVP proxy', () => {
+
+    })
+
+    and('a http post request body', () => {
+
+    })
+
+    when('we post the request to the MVP proxy endpoint unsuccessfully', () => {
+
+    })
+
+    then("the return code will be 500", () => {
+
     })
   })
 
   scenario('Delete proxy succeeds', ({ given, and, when, then }) => {
     given('we have an MVP proxy', () => {
 
-    });
+    })
 
     and('a http post request body', () => {
 
-    });
+    })
 
     when('we post the request to the MVP proxy endpoint successfully', () => {
 
-    });
+    })
 
     and('Then delete the resource with the same identifier', () => {
 
-    });
+    })
 
-    then(/^a get request for a resource with same identifier should give return code (.*)$/, (statusCode: string) => {
+    then("a get request for a resource with same identifier should give return code 404", () => {
 
-    });
-  });
-
-  scenario('Post proxy fails', ({ given, and, when, then }) => {
-    given('we have an MVP proxy', () => {
-
-    });
-
-    and('a http post request body', () => {
-
-    });
-
-    when('we post the request to the MVP proxy endpoint unsuccessfully', () => {
-
-    });
-
-    then(/^the return code will be (.*)$/, (statusCode: string) => {
-
-    });
-  });
+    })
+  })
 
   scenario('Get proxy succeeds', ({ given, and, when, then }) => {
     given('we have an MVP proxy', () => {
 
-    });
+    })
 
     and('a http post request body', () => {
 
-    });
+    })
 
     when('we post the request to the MVP proxy endpoint successfully', () => {
 
-    });
+    })
 
     and('Then get the request with the same identifier', () => {
 
-    });
+    })
 
-    then(/^the return code will be (.*)$/, (statusCode: string) => {
+    then("the return code will be 200", () => {
 
-    });
+    })
 
     and('the content returned should match the content posted', () => {
 
-    });
-  });
+    })
+  })
 })
