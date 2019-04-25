e49c4c3b13c0e7c16fc5f3e586f7816c6ff34872
bryan-yu <bryan-500088@Hotmail.com>
2019-04-25 12:28:57 +0800

    export service url

--- tests/infrastructure/deploy.sh
+++ tests/infrastructure/deploy.sh
@@ -1,10 +1,7 @@
-PROXY_URL=$1
-PROXY_TIMEOUT=$2
-AWS_PROFILE=$3
-VPC_ID=$4
-SUBNET_IDS=$5
+AWS_PROFILE=$1
+
 
 export AWS_SDK_LOAD_CONFIG=true
 
 npm run build
-npm run deploy
+npm run deploy -- --aws-profile ${AWS_PROFILE}
